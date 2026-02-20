01db8e9368e6912344b5877e9e4e5efaf39287df
Hammadmemon2004 <hammadullah20042004@gmail.com>
2026-02-20 16:17:25 +0500

    fixed login page issues

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,65 +1,132 @@
-@extends('front.layouts.app')
-
-<!-- Meta -->
-@section('title', 'Login')
+@extends('admin.auth.layouts.master')
 
 @section('content')
-<div class="container my-5 margin">
-    <div class="row justify-content-center">
-        <div class="col-md-12 col-lg-9">
-            <div class="d-flex rounded-5 shadow sign overflow-hidden">
-                <!-- Left Side: Image and Text -->
-                <div class="sign-in-image col-md-6 d-none d-lg-flex flex-column align-items-center justify-content-start pt-5">
-                    <!-- <h1 class="mt-5 text-custom fw-bolder fs-1">Sign In</h1> -->
-                </div>
-                <!-- Right Side: Form -->
-                <div class="p-4 pb-3 col-lg-6 col-12">
-                    <ul class="nav nav-pills mb-3 justify-content-lg-center justify-content-evenly gap-md-2 border-bottom pb-3 p-3" id="pills-tab" role="tablist">
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link active rounded-pill px-lg-5 px-4 fw-semibold fs-6 sign-btn" id="pills-signin-tab" data-bs-toggle="pill" data-bs-target="#pills-signin" type="button" role="tab" aria-controls="pills-signin" aria-selected="true">Sign In</button>
-                        </li>
-                    </ul>
-                    <form action="{{ route('admin.auth.login') }}" class='p-2 pb-0' method="POST">
-                        @csrf
-                        @method('POST')
-                        <div class="mb-3">
-                            <label for="email" class="form-label fs-6 fw-semibold">Email</label>
-                            <input type="email" name="email" value="{{ old('email') }}" class="form-control rounded-2 border-0 fw-normal form-placeholder" id="email" placeholder="Enter email address..." style='height:43px;background-color:#F8F8F9;' required>
-                        </div>
-                        <div class="mb-3">
-                            <div class="d-flex justify-content-between">
-                                <label for="password" class="form-label fs-6 fw-semibold">Password</label>
-                                <a class="text-decoration-none" href="{{ route('admin.password.request')  }}">
-                                    <small>Forgot Password?</small>
-                                </a>
-                            </div>
-                            <input type="password" name="password" class="form-control rounded-2 border-0 fw-normal form-placeholder" id="password" placeholder="Enter password..."  style='height:43px;background-color:#F8F8F9;' required>
-                        </div>
-                        <div class="d-grid mt-md-0 mt-4">
-                            <button type="submit" class="btn btn-custom rounded-pill fw-semibold fs-6  py-md-0 py-2" style='height:43px;'>Sign In</button>
-                        </div>
-                    </form>
-                    <br>
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <hr class="text-muted" style="border-top: 1px solid #000;">
-                        </div>
-                        <div class="col-auto">
-                            <span class="px-2 bg-white text-muted">Sign In With:</span>
-                        </div>
-                        <div class="col">
-                            <hr class="text-muted" style="border-top: 1px solid #000;">
-                        </div>
-                    </div>
-                    <div class="d-flex flex-column justify-content-center mt-3 gap-3  p-2">
-                        <a href="{{ route('google.login') }}" class="btn rounded-pill w-100 bg-light border-0 p-2 text-black-50 fw-semibold  fs-6">
-                            <i class="fa-brands fa-google"></i> 
-                            Google
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="authentication-wrapper authentication-cover authentication-bg">
+  <div class="authentication-inner row">
+    <!-- /Left Text -->
+    <div class="d-none d-lg-flex col-lg-7 p-0">
+      <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
+        <img
+          src="{!! asset('assets/img/illustrations/auth-login-illustration-light.png') !!}"
+          alt="auth-login-cover"
+          class="img-fluid my-5 auth-illustration"
+          data-app-light-img="illustrations/auth-login-illustration-light.png"
+          data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
+
+        <img
+          src="{!! asset('assets/img/illustrations/bg-shape-image-light.png') !!}"
+          alt="auth-login-cover"
+          class="platform-bg"
+          data-app-light-img="illustrations/bg-shape-image-light.png"
+          data-app-dark-img="illustrations/bg-shape-image-dark.png" />
+      </div>
     </div>
+    <!-- /Left Text -->
+
+    <!-- Login -->
+    <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
+      <div class="w-px-400 mx-auto">
+        <!-- Logo -->
+        <div class="app-brand mb-4">
+          <a href="index.html" class="app-brand-link gap-2">
+            <span class="app-brand-logo demo">
+              <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
+                  fill="#7367F0" />
+                <path
+                  opacity="0.06"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
+                  fill="#161616" />
+                <path
+                  opacity="0.06"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
+                  fill="#161616" />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
+                  fill="#7367F0" />
+              </svg>
+            </span>
+          </a>
+        </div>
+        <!-- /Logo -->
+        <h3 class="mb-1">Welcome to {{ config('app.name', 'Laravel') }}! 👋</h3>
+        <p class="mb-4">Please sign-in to your account and start the adventure</p>
+
+        <form id="formAuthentication" class="mb-3" method="POST" action="{{ route('admin.auth.login') }}">
+            @csrf
+          <div class="mb-3">
+            <label for="email" class="form-label">Email or Username</label>
+            <input
+              type="text"
+              class="form-control"
+              id="email"
+              name="email"
+              placeholder="Enter your email or username"
+              autofocus />
+          </div>
+          <div class="mb-3 form-password-toggle">
+            <div class="d-flex justify-content-between">
+              <label class="form-label" for="password">Password</label>
+              <a href="{{ route('admin.password.request')  }}">
+                <small>Forgot Password?</small>
+              </a>
+            </div>
+            <div class="input-group input-group-merge">
+              <input
+                type="password"
+                id="password"
+                class="form-control"
+                name="password"
+                placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                aria-describedby="password" />
+              <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
+            </div>
+          </div>
+          <div class="mb-3">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="remember-me" />
+              <label class="form-check-label" for="remember-me"> Remember Me </label>
+            </div>
+          </div>
+          <button class="btn btn-primary d-grid w-100">Sign in</button>
+        </form>
+
+        <!-- <p class="text-center">
+          <span>New on our platform?</span>
+          <a href="auth-register-cover.html">
+            <span>Create an account</span>
+          </a>
+        </p> -->
+
+        <!-- <div class="divider my-4">
+          <div class="divider-text">or</div>
+        </div>
+
+        <div class="d-flex justify-content-center">
+          <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
+            <i class="tf-icons fa-brands fa-facebook-f fs-5"></i>
+          </a>
+
+          <a href="javascript:;" class="btn btn-icon btn-label-google-plus me-3">
+            <i class="tf-icons fa-brands fa-google fs-5"></i>
+          </a>
+
+          <a href="javascript:;" class="btn btn-icon btn-label-twitter">
+            <i class="tf-icons fa-brands fa-twitter fs-5"></i>
+          </a>
+        </div> -->
+      </div>
+    </div>
+    <!-- /Login -->
+  </div>
 </div>
 @endsection
